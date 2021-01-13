bdff4224a9515bf6ebe43b57161d44d5baecda9c
Danny <d.berkelaar1@student.avans.nl>
2021-01-13 12:05:10 +0100

    Char* to string

--- server/src/Server.hpp
+++ server/src/Server.hpp
@@ -16,19 +16,18 @@ public:
 
     [[nodiscard]] const std::string &getRootDir() const { return root_dir; }
 
-    [[nodiscard]] const char *getLF() const { return lf; }
+    [[nodiscard]] const std::string &getLF() const { return lf; }
 
-    [[nodiscard]] const char *getCRLF() const { return crlf; }
+    [[nodiscard]] const std::string &getCRLF() const { return crlf; }
 
 private:
 
     std::unique_ptr<asio::ip::tcp::acceptor> server;
     std::unique_ptr<asio::io_context> io_context;
 
-
     const int server_port{12345};
-    const char *lf{"\n"};
-    const char *crlf{"\r\n"};
+    const std::string lf{"\n"};
+    const std::string crlf{"\r\n"};
     const std::string root_dir{"./server_dir/"};
 
     void handleClient();
